3c81c04a9591fd26da16ebcaaff8ac83dfda9522
Oleg Goyarchuk <oleh.g>
2023-04-05 17:27:50 +0000

    1 script

---
+++ ans.sh
@@ -0,0 +1,11 @@
+sudo apt update
+
+sudo apt install -y python3 python3-pip # Install Ansible
+
+sudo apt-add-repository -y ppa:ansible/ansible
+
+sudo apt update
+
+sudo apt install -y ansible # Verify Ansible installation
+
+ansible --version
